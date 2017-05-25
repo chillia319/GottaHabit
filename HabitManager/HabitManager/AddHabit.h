282f9999e15106a288c12ce3003d16cc3e06c949
TylerMB <tyler.max.baker@gmail.com>
2017-05-25 21:45:31 +1200

    mutator and accessor methods added

---
+++ HabitManager/HabitManager/AddHabit.h
@@ -0,0 +1,26 @@
+//
+//  AddHabit.h
+//  HabitManager
+//
+//  Created by Tyler Baker on 23/05/17.
+//  Copyright © 2017 Percy Hu. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+
+#ifdef __cplusplus
+extern "C" int accessCPP()
+{
+#endif
+    
+    
+    int checkTest();
+    
+    
+    
+#ifdef __cplusplus
+}
+#endif
+
