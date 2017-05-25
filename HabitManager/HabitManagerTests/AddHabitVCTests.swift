8712f1b719f23f89cc6ad73e08eb3fccc107216f
Alexis Barltrop <alexisbarltrop@gmail.com>
2017-05-26 10:22:07 +1200

    travis testing

--- HabitManager/HabitManagerTests/AddHabitVCTests.swift
+++ HabitManager/HabitManagerTests/AddHabitVCTests.swift
@@ -22,7 +22,7 @@ class AddHabitVCTests: XCTestCase {
         super.tearDown()
     }
     //Want ot check that the save button is disabled if the description is empty
-    func testCheckDescriptionFalse() {
+    func CheckDescriptionFalse() {
         let habitVC = AddHabitVC()
         
         habitVC.habitDescription.text = ""
