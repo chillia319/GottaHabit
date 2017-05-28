ef76a6648469a41b281c834b356f28c11a0242e2
Alexis Barltrop <alexisbarltrop@gmail.com>
2017-05-28 12:20:48 +1200

    Add UI Tests

--- HabitManager/HabitManagerUITests/HabitManagerUITests.swift
+++ HabitManager/HabitManagerUITests/HabitManagerUITests.swift
@@ -2,7 +2,7 @@
 //  HabitManagerUITests.swift
 //  HabitManagerUITests
 //
-//  Created by Alexis Barltrop on 5/26/17.
+//  Created by Alexis Barltrop on 5/27/17.
 //  Copyright © 2017 Percy Hu. All rights reserved.
 //
 
@@ -19,7 +19,6 @@ class HabitManagerUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,16 +27,22 @@ class HabitManagerUITests: XCTestCase {
         super.tearDown()
     }
     
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
     func testAddOneDailyHabit() {
         
         let app = XCUIApplication()
         let table = app.tables.element(boundBy: 0)
         let numRows = table.cells.count
-
+        
         app.navigationBars["All Habits"].buttons["Add"].tap()
         app.tables.buttons["Daily"].tap()
         
         let tablesQuery = app.tables
+        print(tablesQuery)
         tablesQuery.textFields["Stand up for 2 minutes"].tap()
         tablesQuery.children(matching: .cell).element(boundBy: 1).children(matching: .textField).element.typeText("Daily")
         app.buttons["Done"].tap()
@@ -47,9 +52,60 @@ class HabitManagerUITests: XCTestCase {
         let expectedRows = numRows+1
         
         XCTAssertEqual(table.cells.count, expectedRows)
-        
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    /**
+     *  Manually adds 100 empty habits - no alerts.
+     *
+     */
+    
+    func Add100Habits(){
+        let app = XCUIApplication()
+        let habitString = "Habit"
+        let upperLimit = 1000
+        var habitNumber = 0
+        
+        while habitNumber < upperLimit {
+        app.navigationBars["All Habits"].buttons["Add"].tap()
+        //app.tables.buttons["Daily"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.textFields["Stand up for 2 minutes"].tap()
+        tablesQuery.children(matching: .cell).element(boundBy: 1).children(matching: .textField).element.typeText(habitString+String(habitNumber))
+        app.typeText("\r")
+            
+        
+        app.navigationBars["New Habit"].buttons["Save"].tap()
+        
+        habitNumber += 1
+        }
+        
+
+    }
+    
+    func testDeleteOneDailyHabit(){
+        
+        let app = XCUIApplication()
+        let table = app.tables.element(boundBy: 0)
+        let numRows = table.cells.count
+        
+        app.navigationBars["All Habits"].buttons["Add"].tap()
+        let tablesQuery = app.tables
+        tablesQuery.textFields["Stand up for 2 minutes"].tap()
+        
+        let textField = tablesQuery.children(matching: .cell).element(boundBy: 1).children(matching: .textField).element
+        textField.typeText("DailyTestHabit")
+        
+        app.buttons["Done"].tap()
+        app.navigationBars["New Habit"].buttons["Save"].tap()
+        
+        
+        tablesQuery.staticTexts["DailyTestHabit"].swipeLeft()
+        tablesQuery.buttons["Delete"].tap()
+        
+        XCTAssertEqual(table.cells.count, numRows)
+    }
+    
+    
     
 }
