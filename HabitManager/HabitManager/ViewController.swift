c9d4ec1c39badbedc55fee485e4609cdded871d2
Percy Hu <powerofpercy@outlook.com>
2017-09-26 20:17:52 +1300

    Embracing the search bar

--- HabitManager/HabitManager/ViewController.swift
+++ HabitManager/HabitManager/ViewController.swift
@@ -123,7 +123,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     cell.cellSwitch.isUserInteractionEnabled = false
                     cell.habitLabel.textColor = UIColor(red:0.259, green:0.259, blue:0.259, alpha:1.000)
                     cell.habitDetailsLabel.textColor = UIColor(red:0.369, green:0.369, blue:0.369, alpha:1.000)
-                    cell.backgroundColor = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1.0)
+                    let expiredColour = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1.0)
+                    cell.backgroundColor = expiredColour
                 }else{
                     cell.cellSwitch.isUserInteractionEnabled = true
                 }
@@ -293,8 +294,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     cell.cellSwitch.isUserInteractionEnabled = false
                     cell.habitLabel.textColor = UIColor(red:0.259, green:0.259, blue:0.259, alpha:1.000)
                     cell.habitDetailsLabel.textColor = UIColor(red:0.369, green:0.369, blue:0.369, alpha:1.000)
-                    cell.backgroundColor = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1.0)
-                    colours[indexPath.row] = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1.0)
+                    let expiredColour = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1.0)
+                    cell.backgroundColor = expiredColour
+                    colours[indexPath.row] = expiredColour
                 }else{
                     cell.cellSwitch.isUserInteractionEnabled = true
                 }
@@ -496,7 +498,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         UISearchBar.appearance().barTintColor = UIColor(red: 67/255, green: 66/255, blue: 64/255, alpha: 1.0)
         UISearchBar.appearance().tintColor = .white
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = UIColor(red: 67/255, green: 66/255, blue: 64/255, alpha: 1.0)
-        habitsTable.setContentOffset(CGPoint(x:0, y:self.searchController.searchBar.frame.size.height), animated: false)
+        //habitsTable.setContentOffset(CGPoint(x:0, y:self.searchController.searchBar.frame.size.height), animated: false)
         definesPresentationContext = true
     }
     
